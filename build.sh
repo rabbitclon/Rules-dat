a2b527faea9265bb46dcd7a5611447a51782be91
rabbitclon <98285270+rabbitclon@users.noreply.github.com>
2026-05-10 20:42:09 +0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,26 +2,35 @@
 set -e
 
 WORKDIR=work
+XRAYDIR=xray
+
 rm -rf $WORKDIR
+rm -rf $XRAYDIR
+
 mkdir -p $WORKDIR
+mkdir -p $XRAYDIR
+
 cd $WORKDIR
 
 echo "Download Loyalsoldier geosite..."
 wget -q https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geosite.dat -O geosite.dat
 
-echo "Download Xray core..."
-wget -q https://github.com/XTLS/Xray-core/releases/latest/download/Xray-linux-64.zip
-unzip -q Xray-linux-64.zip
+cd ..
 
-XRAY=$(find . -type f -name xray | head -n 1)
+echo "Download Xray core..."
+wget -q https://github.com/XTLS/Xray-core/releases/latest/download/Xray-linux-64.zip -O xray.zip
+
+unzip -oq xray.zip -d $XRAYDIR
+
+XRAY=$(find $XRAYDIR -type f -name xray | head -n 1)
 chmod +x "$XRAY"
 
-echo "Preparing custom rules..."
+echo "Prepare custom rules..."
 
-TM=$(grep -v '^$' ../custom/tm-rules.txt | sed 's/^/"/;s/$/"/' | paste -sd, -)
-PROXY=$(grep -v '^$' ../custom/proxy-rules.txt | sed 's/^/"/;s/$/"/' | paste -sd, -)
+TM=$(grep -v '^$' custom/tm-rules.txt | sed 's/^/"/;s/$/"/' | paste -sd, -)
+PROXY=$(grep -v '^$' custom/proxy-rules.txt | sed 's/^/"/;s/$/"/' | paste -sd, -)
 
-cat > geosite_custom.json <<EOF
+cat > custom.json <<EOF
 {
   "version": 1,
   "rules": [
@@ -31,17 +40,17 @@ cat > geosite_custom.json <<EOF
     },
     {
       "domain": [$PROXY],
-      "outboundTag": "proxy"
+      "outboundTag": "proxy-rules"
     }
   ]
 }
 EOF
 
-echo "Merging via Xray geo..."
+echo "Build geosite..."
 
 $XRAY run geo \
-  --input-file=geosite.dat \
-  --domain-file=geosite_custom.json \
-  --output=geosite-custom.dat
+  --input-file=work/geosite.dat \
+  --domain-file=custom.json \
+  --output=work/geosite-custom.dat
 
-echo "Done"
+echo "Done!"
